6e192edaccc9f4ecd75bad7a06665d0652aec099
Anastasiya Nislovskaya <anastasiya@MacBook-Pro-Anastasiya.local>
2024-10-28 00:30:27 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,69 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("error opening source file: %w", err)
+	}
+	defer srcFile.Close()
+
+	limit, err = setLimit(srcFile, offset, limit)
+	if err != nil {
+		return fmt.Errorf("error setting limit: %w", err)
+	}
+
+	_, err = srcFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("error seeking in source file: %w", err)
+	}
+
+	destFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("error creating destination file: %w", err)
+	}
+	defer destFile.Close()
+
+	n, err := copyWithProgress(srcFile, destFile, limit)
+	if err != nil {
+		return fmt.Errorf("error writing to destination file: %w", err)
+	}
+
+	fmt.Printf("\nCopied %d bytes from %s to %s\n", n, fromPath, toPath)
 	return nil
+}
+
+func setLimit(src *os.File, offset, limit int64) (int64, error) {
+	fileInfo, err := src.Stat()
+	if err != nil {
+		return 0, fmt.Errorf("error getting file info: %w", err)
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return 0, ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if offset > fileSize {
+		return 0, ErrOffsetExceedsFileSize
+	}
+	if limit <= 0 || limit > fileSize-offset {
+		limit = fileSize - offset
+	}
+
+	return limit, nil
+}
+
+func copyWithProgress(src io.Reader, dest io.Writer, limit int64) (int64, error) {
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(src)
+
+	n, err := io.CopyN(dest, barReader, limit)
+	bar.SetTotal(limit)
+	bar.Finish()
+
+	if err != nil {
+		return n, fmt.Errorf("error writing to destination: %w", err)
+	}
+
+	return n, nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,15 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/nislovskaya/go_prof_course/hw07_file_copying
 
 go 1.22
+
+require github.com/cheggaaa/pb/v3 v3.1.5
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
