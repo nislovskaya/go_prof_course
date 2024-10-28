352f77c912612a5caeb33aa8fd18295e1e4faea9
Anastasiya Nislovskaya <anastasiya@MacBook-Pro-Anastasiya.local>
2024-10-28 03:12:40 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,54 @@
 package main
 
-import "testing"
+import (
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	dir := "testdata/env"
+
+	env, err := ReadDir(dir)
+	require.NoError(t, err, "unexpected error while reading env")
+
+	testCases := []struct {
+		name     string
+		cmd      []string
+		env      Environment
+		expected int
+	}{
+		{
+			name:     "Valid arguments",
+			cmd:      []string{"ls"},
+			env:      env,
+			expected: 0,
+		},
+		{
+			name:     "Valid env",
+			cmd:      []string{"ls", "-l"},
+			env:      Environment{},
+			expected: 0,
+		},
+		{
+			name:     "Command is not provided",
+			cmd:      []string{},
+			env:      Environment{},
+			expected: int(syscall.EINVAL),
+		},
+		{
+			name:     "Invalid command",
+			cmd:      []string{"invalid_command"},
+			env:      env,
+			expected: 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			exitCode := RunCmd(tc.cmd, tc.env)
+			require.Equal(t, tc.expected, exitCode)
+		})
+	}
 }
